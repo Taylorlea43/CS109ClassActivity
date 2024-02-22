642a163979d39a6dc22cf618bcf6e8c02e42d083
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-21 19:15:29 -0500

    added new println

--- classActivity.java
+++ classActivity.java
@@ -2,6 +2,6 @@ public class classActivity{
 
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("Hello world!!")
+        System.out.println("Hello world!!");
     }
 }
